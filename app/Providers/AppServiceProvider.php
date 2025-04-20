3669b09bb6f78bb4490d9ede238e388b0e00875a
John Ashley Villanueva <villanuevajohn519@gmail.com>
2025-04-21 02:50:24 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Support\ServiceProvider;
 use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +22,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Kernel $kernel): void
     {
+        // Set mail configuration dynamically
+        Config::set('mail.default', 'smtp');
+        Config::set('mail.mailers.smtp', [
+            'transport' => 'smtp',
+            'host' => 'smtp.gmail.com',
+            'port' => 465,
+            'encryption' => 'ssl',
+            'username' => 'villanuevajohn519@gmail.com',
+            'password' => 'sqiyqwyejqecypzl',
+            'timeout' => null,
+        ]);
+        Config::set('mail.from', [
+            'address' => 'villanuevajohn519@gmail.com',
+            'name' => 'Please Check The Mail',
+        ]);
+
         // Optionally, if you want to register RoleMiddleware globally:
         // $kernel->appendMiddlewareToGroup('web', RoleMiddleware::class);
-
-        // You can add other middleware or configurations here if needed
     }
 }
-
--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+'default' => env('MAIL_MAILER', 'sendmail'), // Change from 'log' to 'sendmail'
 
     /*
     |--------------------------------------------------------------------------
