52b7bda87eef278634b76df711eee42bccf3ab5d
John Ashley Villanueva <villanuevajohn519@gmail.com>
2025-04-21 01:24:35 +0800

    update

--- database/migrations/2025_04_20_171759_add_property_no_to_items_table.php
+++ database/migrations/2025_04_20_171759_add_property_no_to_items_table.php
@@ -10,17 +10,22 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-{
-    Schema::table('items', function (Blueprint $table) {
-        $table->string('property_no')->nullable()->after('or_date');
-        $table->string('classification_no')->nullable()->after('property_no');
-    });
-}
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->string('property_no')->nullable()->after('or_date');
+            $table->string('classification_no')->nullable()->after('property_no');
+            $table->date('date_purchase')->nullable()->after('classification_no');
+        });
+    }
 
-public function down()
-{
-    Schema::table('items', function (Blueprint $table) {
-        $table->dropColumn(['property_no', 'classification_no']);
-    });
-}
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn([
+                'property_no',
+                'classification_no',
+                'date_purchase'
+            ]);
+        });
+    }
 };
