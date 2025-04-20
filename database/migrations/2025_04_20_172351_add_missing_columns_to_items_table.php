3de32668de8e633a29c51b42e2708f7386e1fa50
John Ashley Villanueva <villanuevajohn519@gmail.com>
2025-04-21 01:31:26 +0800

    update

--- database/migrations/2025_04_20_172351_add_missing_columns_to_items_table.php
+++ database/migrations/2025_04_20_172351_add_missing_columns_to_items_table.php
@@ -9,20 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            //
+            $table->string('property_no')->nullable()->after('or_date');
+            $table->string('classification_no')->nullable()->after('property_no');
+            $table->date('date_purchase')->nullable()->after('classification_no');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('items', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'property_no',
+                'classification_no',
+                'date_purchase'
+            ]);
         });
     }
 };
